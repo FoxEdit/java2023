161e09d9d87bffc30af263a5afdb219d51fe5526
FoxEdit <foxeditt@gmail.com>
2023-10-20 11:30:33 +0300

    7

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
+
 public class Main {
     public static void main(String[] args) {
-        SteelFork fork = new SteelFork();
-        GlassBottle bottle = new GlassBottle();
-        WoodenPlate plate = new WoodenPlate();
-
-        System.out.println("fork: " + fork.materialImpactSound() +
-                "\nbottle: " + bottle.materialImpactSound() +
-                "\nplate: " + plate.materialImpactSound());
+        FootballMatchSimulator simulator = new FootballMatchSimulator();
+        simulator.setVisible(true);
     }
 }
