343ac882980acf7b8f41c6294eadb302660e5426
FoxEdit <foxeditt@gmail.com>
2023-10-20 11:35:31 +0300

    10_3

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,17 @@
-
 public class Main {
     public static void main(String[] args) {
-        String number = "001111110";
-        int count = countOnesInSequence(number, 0, 0);
-        System.out.println("Число 1 встречается " + count + " раз(а) в последовательности.");
+        int N = 12345;
+        printDigitsReverse(N);
     }
 
-    public static int countOnesInSequence(String input, int index, int onesCount) {
-        if (index >= input.length()) {
-            // Достигнут конец ввода, возвращаем количество единиц.
-            return onesCount;
+    public static void printDigitsReverse(int N) {
+        if (N == 0) {
+            return;
+        } else {
+            int lastDigit = N % 10;
+            N /= 10;
+            System.out.println(lastDigit); // Выводим цифру
+            printDigitsReverse(N);
         }
-
-        if (index + 1 < input.length()
-                && input.substring(index, index + 2).equals("00")) {
-            // Если найдены два нуля подряд, завершаем счет
-            return onesCount;
-        }
-
-        char currentChar = input.charAt(index);
-
-        if (currentChar == '1') {
-            // Если текущий символ - '1', увеличиваем счетчик единиц.
-            onesCount++;
-        }
-
-        // Рекурсивный вызов для следующего символа.
-        return countOnesInSequence(input, index + 1, onesCount);
     }
 }
